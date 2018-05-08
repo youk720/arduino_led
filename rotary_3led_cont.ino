61583679b9e0b91e24269e1c72c384a566e869a2
youk233_231 <yuuki_16n1100449@nnn.ed.jp>
2018-05-08 16:03:44 +0900

    led追加
    ※エラー発生中

--- rotary_3led_cont.ino
+++ rotary_3led_cont.ino
@@ -1,83 +1,107 @@
-#define rotary_a 2
-#define rotary_b_left 4
-#define rotary_b_cent 7 //準備用
-#define rotary_b_right 8 //準備用
- byte r;
- byte g;
- byte b;
+#define ENC_A  2
+#define ENC_B  4
  
-
-void setup(){
-  pinMode(rotary_a, INPUT_PULLUP); // INPUT_PULLUPを使用することでマイコン内のプルアップ抵抗が有効になる
-  pinMode(rotary_b_left, INPUT_PULLUP);
-  pinMode(rotary_b_cent, INPUT_PULLUP); //準備用
-  pinMode(rotary_b_right, INPUT_PULLUP);  //準備用
-  pinMode(9, OUTPUT); //円LED(青)のピンに設定
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
+volatile byte pos;
+volatile int  enc_count;
+ 
+#define LED_SW  A0
+#define LED_SIZE 3
+const byte led_pin[LED_SIZE]  = {10, 11, 9};   // digital pins for LED
+byte       led_luma[LED_SIZE] = {255, 255}; // luma for each LED
+boolean    sw = false;
+ 
+ 
+void setup() {
+  pinMode(ENC_A, INPUT_PULLUP);
+  pinMode(ENC_B, INPUT_PULLUP);
+ 
+  attachInterrupt(0, ENC_READ, CHANGE);
+  attachInterrupt(1, ENC_READ, CHANGE);
+ 
   Serial.begin(9600);
+ 
+  for (byte i = 0 ; i < LED_SIZE ; i++)
+  {
+    pinMode(led_pin[i], OUTPUT);
+    analogWrite(led_pin[i], led_luma[i]);
   }
-
-void loop(){
-    //エンコーダの値を取得するための変数宣言
-  byte rotary_plus = 0;
-  byte rotary_minus_L = 0;
-  byte rotary_minus_C = 0;
-  byte rotary_minus_R = 0;
-//各エンコーダの状態取得
-  rotary_plus = digitalRead(rotary_a);
-  rotary_minus_L = digitalRead(rotary_b_left);
-  rotary_minus_C = digitalRead(rotary_b_cent);
-  rotary_minus_R = digitalRead(rotary_b_right);
-
-   //赤用  
-
-  if (rotary_plus == LOW) {
-    r = constrain(r, 0, 250);
-    r = r + 5;
-    if (r == -5){
-      r = 0;
-    }
-    } else if(rotary_minus_L == LOW){
-      r = constrain(r, 5, 250);
-      r = r - 5;
-      }
-      analogWrite(9, g);
-      analogWrite(3, g);
-  
-  //緑用
-  //a側に回った時か否かチェック
-  if (rotary_plus == LOW) {
-    g = constrain(g, 0, 250);
-    g = g + 5;
-    if (g == -5){
-      g = 0;
-    }
-    } else if(rotary_minus_C == LOW){
-      g = constrain(g, 5, 250);
-      g = g - 5;
-      }
-      analogWrite(10, g);
-      analogWrite(5, g);
-
-  //青用
-  //a側に回った時か否かチェック
-  if (rotary_plus == LOW) {
-    b = constrain(b, 0, 250);
-    b = b + 5;
-    if (b == -5){
-      b = 0;
-    }
-    } else if(rotary_minus_R == LOW){
-      b = constrain(b, 5, 250);
-      b = b - 5;
-      }
-      analogWrite(11, b);
-      analogWrite(6, b);
-      Serial.println(b);
-      delay(50);
+  pinMode(LED_SW, INPUT_PULLUP);
+  SERIAL_MON();
+}
+ 
+ 
+void loop() {
+ 
+  // LED switch change
+  unsigned long gauge;
+  while (!digitalRead(LED_SW)) gauge++;
+  if (gauge > 700)
+  {
+    sw = !sw;
+    SERIAL_MON();
   }
-  
+ 
+  // set led_luma of active LED
+  short led_ref = ENC_COUNT(led_luma[sw]);
+  led_ref = constrain(led_ref, 0, 255);
+ 
+  // excute luma change
+  if (led_luma[sw] != led_ref)
+  {
+    led_luma[sw] = led_ref;
+    analogWrite(led_pin[sw], led_luma[sw]);
+    SERIAL_MON();
+  }
+}
+ 
+ 
+void SERIAL_MON() {
+  for (byte i = 0 ; i < LED_SIZE ; i++)
+  {
+    Serial.print((char)(i + 'A'));
+    Serial.print(":");
+    Serial.print(led_luma[i]);
+    if (sw == i) Serial.print("< ");
+    else Serial.print("  ");
+  }
+  Serial.println();
+}
+ 
+ 
+int ENC_COUNT(int incoming) {
+  static int enc_old = enc_count;
+  int val_change = enc_count - enc_old;
+ 
+  if (val_change != 0)
+  {
+    incoming += val_change;
+    enc_old   = enc_count;
+  }
+  return incoming;
+}
+ 
+ 
+void ENC_READ() {
+  byte cur = (!digitalRead(ENC_B) << 1) + !digitalRead(ENC_A);
+  byte old = pos & B00000011;
+  byte dir = (pos & B00110000) >> 4;
+ 
+  if (cur == 3) cur = 2;
+  else if (cur == 2) cur = 3;
+ 
+  if (cur != old)
+  {
+    if (dir == 0)
+    {
+      if (cur == 1 || cur == 3) dir = cur;
+    } else {
+      if (cur == 0)
+      {
+        if (dir == 1 && old == 3) enc_count++;
+        else if (dir == 3 && old == 1) enc_count--;
+        dir = 0;
+      }
+    }
+    pos = (dir << 4) + (old << 2) + cur;
+  }
+}
