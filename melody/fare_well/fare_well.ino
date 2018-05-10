fd1533296e6ddb950d7bb34dca3418eb526f0e34
youk233_231 <yuuki_16n1100449@nnn.ed.jp>
2018-05-10 03:18:36 +0000

    すみれの花咲く頃_追加打ち込み

--- melody/fare_well/fare_well.ino
+++ melody/fare_well/fare_well.ino
@@ -5,6 +5,51 @@
 
 
 //ドレミを定義
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
 #define NOTE_GS4 415
 #define NOTE_A4  440
 #define NOTE_AS4 466
@@ -58,11 +103,12 @@ void setup() {
 }
 
 void loop() {
-  if (analogRead(A0) == HIGH){
-    tobu();
-     } else if(analogRead(A1) == HIGH){
-      summer_color();
-      }
+//  if (analogRead(A0) == HIGH){
+//    sumire();
+//     } else if(analogRead(A1) == HIGH){
+//      summer_color();
+//      }
+sumire();
   }
 
 void summer_color(){ //夏色の時間 JR新宿駅4番線発車メロディ
@@ -137,6 +183,170 @@ void summer_color(){ //夏色の時間 JR新宿駅4番線発車メロディ
     delay(100);
 
     }
+  void sumire(){
+    //低音ver
+//    tone(pin,NOTE_D4,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_C4,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_AS3,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_D4,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_F4,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_F3,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_A3,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_AS3,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_D4,one_haku);
+//    delay(one_haku);
+//    tone(pin,NOTE_G4,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_F3,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_A3,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_AS3,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_D4,one_haku);
+//    delay(one_haku);
+//    tone(pin,NOTE_G4,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_F3,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_A3,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_AS3,eight_haku);
+//    delay(eight_haku);
+//    tone(pin,NOTE_D4,one_haku);
+//    delay(one_haku);
+//   tone(pin,NOTE_A4,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_F3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_A3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_AS3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_F4,one_haku);
+//   delay(one_haku);
+//   tone(pin,NOTE_D4,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_F3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_A3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_AS3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_D4,one_haku);
+//   delay(one_haku);
+//   tone(pin,NOTE_F3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_D3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_F3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_A3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_D4,one_haku);
+//   delay(one_haku);
+//   tone(pin,NOTE_AS3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_F3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_A3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_AS3,eight_haku);
+//   delay(eight_haku);
+//   tone(pin,NOTE_D4,one_haku);
+//   delay(one_haku);
+    
+    //高音ver
+    tone(pin,NOTE_D6,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_C6,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_AS5,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_D6,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_F6,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_F5,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_A5,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_AS5,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_D6,one_haku);
+    delay(one_haku);
+    tone(pin,NOTE_G6,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_F5,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_A5,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_AS5,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_D6,one_haku);
+    delay(one_haku);
+    tone(pin,NOTE_G6,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_F5,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_A5,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_AS5,eight_haku);
+    delay(eight_haku);
+    tone(pin,NOTE_D6,one_haku);
+    delay(one_haku);
+   tone(pin,NOTE_A6,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_F5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_A5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_AS5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_F6,one_haku);
+   delay(one_haku);
+   tone(pin,NOTE_D6,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_F5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_A5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_AS5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_D6,one_haku);
+   delay(one_haku);
+   tone(pin,NOTE_F5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_D5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_F5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_A5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_D6,one_haku);
+   delay(one_haku);
+   tone(pin,NOTE_AS5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_F5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_A5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_AS5,eight_haku);
+   delay(eight_haku);
+   tone(pin,NOTE_D6,one_haku);
+   delay(one_haku);
+
+    delay(500);
+    }
+
 //   void fare()  { //farewell JR新宿駅2番線発車メロディ https://youtu.be/pE9fJyBUxns?t=1m2s
 //      tone(pin,NOTE_F5,one_haku); //ファ
 //      delay(one_haku);
@@ -234,3 +444,4 @@ void summer_color(){ //夏色の時間 JR新宿駅4番線発車メロディ
 //
 //   delay(500);
 //   }
+                                                                                                                                                                                                                  
