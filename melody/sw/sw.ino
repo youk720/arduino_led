cf84eb04568a8696e668e90007e939b72c5bf872
youk233_231 <yuuki_16n1100449@nnn.ed.jp>
2018-05-11 05:46:30 +0000

    スイッチx13実験スケッチ作成_成功

--- melody/sw/sw.ino
+++ melody/sw/sw.ino
@@ -1,39 +1,68 @@
-#define led_1 3  //LEDのピン定義
-#define led_2 4
-#define led_3 5
-#define led_4 6
 #define sw_1 A0 //スイッチのピン定義 
 #define sw_2 A1 //鍵盤のスイッチは計13個あるので4つ一ピンで分圧回路作成
 #define sw_3 A2
 #define sw_4 A3
 
+
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(led_1, OUTPUT); //LEDピンをoutput
-  pinMode(led_2, OUTPUT);
-  pinMode(led_3, OUTPUT);
-  pinMode(led_4, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  int value_1 = analogRead(sw_1); スイッチピン1番の定義
-//  value = value % 1000;
-  Serial.println(value_1);
+  int sw = analogRead(sw_1); //ピン1番の定義
+//  Serial.println(sw);
+  int x;
 
-  if(value_1 >= 700 && value_1 <= 800){ //sw4の場合
-    digitalWrite(led_1, HIGH); //青作動
-  } else if (value_1 >= 600 && value_1 <= 700){ //sw3の場合
-    digitalWrite(led_2, HIGH); //黄色作動
-    } else if(value_1 >=500 && value_1 <= 600){ //sw2の場合
-      digitalWrite(led_3, HIGH); //赤作動
-      } else if (value_1 == 0){ //sw1の場合
-        digitalWrite(led_4, HIGH); //緑作動
-        } else { //上の処理以外なら非動作
-         digitalWrite(led_1, LOW);
-         digitalWrite(led_2, LOW);
-         digitalWrite(led_3, LOW); 
-         digitalWrite(led_4, LOW); 
-          }
-  
+  String key[] = {
+    "non touch", "C# or D♭", "D# or E♭", "F# or G♭", "G# or A♭", "A# or B♭", "C+", "B", "A", "G", "F", "E", "D", "C-"
+};
+    
+      if(sw >=940 && sw <= 960){
+      x = 13; 
+      }else if(sw >= 936 && sw <= 940){
+      x = 12;
+      }else if(sw >= 925 && sw <= 934){
+      x = 11;
+      }else if(sw >= 916 && sw <= 926){
+      x = 10;
+      }else if(sw >= 900 && sw <= 915){
+      x = 9;
+      }else if(sw >= 885 && sw <= 899){
+      x = 8;
+      }else if(sw >= 860 && sw <= 880){
+      x = 7;
+      }else if(sw >= 850 && sw <= 865){
+      x = 6;
+      }else if(sw >= 800 && sw <= 830){
+      x = 5;
+      }else if(sw >= 700 && sw <= 800){
+      x = 4;
+      }else if(sw >= 600 && sw <= 700){
+      x = 3;
+      }else if(sw >= 500 && sw <= 600){
+      x = 2;
+      }else if(sw >= 0 && sw <= 10){
+      x = 1;
+      }
+      Serial.println(x);
+      Serial.println(key[x]);
+      delay(500);
+    /*シリアル値測定結果
+     * sw1C#=0~10
+     * 2D#=500~600
+     * 3F#=600~700
+     * 4G#=700~800
+     * 5A#=800~830
+     * 6C+=850~865
+     * 7B=860~880
+     * 8A=885~899
+     * 9G=900~915
+     * 10F=916~926
+     * 11E=925~934
+     * 12D=936~940
+     * 13C=940~960
+     * 
+     */
 }
